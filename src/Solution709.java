b3b8347b5b1051f0a8f295962939de7a509e0d90
guoliang <guoliang@hrtpayment.com>
2018-09-20 14:43:48 +0800

    709. To Lower Case

---
+++ src/Solution709.java
@@ -0,0 +1,8 @@
+// 709. To Lower Case
+// https://leetcode.com/problems/to-lower-case/
+public class Solution709 {
+    public String toLowerCase(String str) {
+        if (str == null) return str;
+        return str.toLowerCase();
+    }
+}
